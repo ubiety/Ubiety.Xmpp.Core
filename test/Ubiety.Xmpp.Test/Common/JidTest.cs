69cb783aed780810a250611b8abcf15598f4f4d0
Dieter Lunn <coder2000@gmail.com>
2019-06-05 00:24:13 -0500

    test: add more jid tests

--- test/Ubiety.Xmpp.Test/Common/JidTest.cs
+++ test/Ubiety.Xmpp.Test/Common/JidTest.cs
@@ -25,10 +25,43 @@ namespace Ubiety.Xmpp.Test.Common
         }
 
         [Fact]
-        public void EscapeUsername()
+        public void EscapeUsernameWithApostrophe()
         {
             var jid = Jid.Parse("d'artangan@garcon.fr/testing");
             jid.Id.Should().Be(@"d\27artangan@garcon.fr/testing");
+            jid.User.Should().Be(@"d'artangan");
+        }
+
+        [Fact]
+        public void EscapeUsernameWithAt()
+        {
+            var jid = new Jid("me@you", "testing.com");
+            jid.Id.Should().Be(@"me\40you@testing.com");
+        }
+
+        [Fact]
+        public void EscapeUsernameWithAtFromParse()
+        {
+            var jid = Jid.Parse("me@you@testing.com");
+            jid.Id.Should().Be(@"me\40you@testing.com");
+        }
+
+        [Fact]
+        public void EqualsWithDifferentObjects()
+        {
+            var left = Jid.Parse("test@test.com");
+            var right = new Jid("test", "test.com");
+
+            left.Should().Be(right);
+        }
+
+        [Fact]
+        public void EqualsTheSameObject()
+        {
+            var jid = Jid.Parse("test@test.com");
+            var clone = jid;
+
+            jid.Should().BeSameAs(clone);
         }
     }
 }
